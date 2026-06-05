a0b5ba548d28b93f881cfc503eec70d3bdf8fa0e
micnori <micnori@users.noreply.github.com>
2026-06-05 14:46:38 +0200

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -48,9 +48,11 @@ if __name__ == "__main__":
     init_model(context, lang="en", device="cpu")
     logger.info("Model initialized successfully.")
 
-    lpcm_bytes, sample_rate = read_audio_to_lpcm_bytes("test/file.wav", sample_rate=16000, bit_depth=16)
+    lpcm_bytes, sample_rate = read_audio_to_lpcm_bytes("test/file.wav", sample_rate=16000, bit_depth=16, mono=True)
     
     logger.info("Starting inference handler with chunked processing...")
     # Processa in chunk di 2048 byte (circa 128 ms a 16 kHz con 16-bit mono)
-    chunk_size = 2048
+    # Processa in chunk di 4096  byte (circa 256 ms a 16 kHz con 16-bit mono)
+    # Processa in chunk di 8192  byte (circa 512 ms a 16 kHz con 16-bit mono)    
+    chunk_size = 8192
     results = process_audio_in_chunks(context, lpcm_bytes, chunk_size, sample_rate)    
